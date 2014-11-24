09a5d9fcb751e358c765a9625d82550cca14e7dd
n.dolgoszyja <n.dolgoszyja@samsung.com>
2014-11-24 16:14:32 +0100

    pom

--- src/main/java/mgr/Main.java
+++ src/main/java/mgr/Main.java
@@ -1,9 +1,12 @@
 package mgr;
 
+import edu.uci.ics.jung.algorithms.generators.random.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+       
     }
 
 }
